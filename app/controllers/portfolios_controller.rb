a297e9620ee3d823714e298a8266a256c9cd56f7
MulanSales <luanmsacc@gmail.com>
2019-02-24 10:48:54 -0300

    implemented destroy action for portfolio deature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -43,4 +43,14 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
   end
 
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+
+    @portfolio_item.destroy
+
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Portfolio record was removed'}
+    end
+  end
+
 end
