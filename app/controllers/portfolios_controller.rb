342ca2d73eeb4aa8be0829e8b10ae5705942e7f7
MulanSales <luanmsacc@gmail.com>
2019-02-27 11:14:42 -0300

    implemented custom scopes for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,7 +2,11 @@ class PortfoliosController < ApplicationController
 
   # GET /portfolios
   def index
-    @portfolio_items = Portfolio.all 
+    @portfolio_items = Portfolio.all
+  end
+
+  def angular
+    @angular_portfolio_items = Portfolio.angular
   end
 
   # GET /portfolios/new
@@ -12,7 +16,7 @@ class PortfoliosController < ApplicationController
 
   # POST /portfolio
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitile, :body))
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
     respond_to do |format|
       if @portfolio_item.save
@@ -31,7 +35,7 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
 
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitile, :body))
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'Your portfolio has been updated'}
       else
         format.html { render :edit }
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+    )
+end
+
+puts "3 topics created"
+
+
 for i in 0..9
   Blog.create!(
     title: "My Blog Post #{i}",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel nunc justo. Vestibulum blandit et sem quis auctor. Sed elementum, sapien sit amet posuere cursus, orci tortor feugiat enim, et hendrerit lacus lectus nec purus. Nunc sed nunc eget velit interdum pellentesque id vel erat. Nunc dictum libero mollis semper eleifend. Praesent hendrerit metus id justo ultrices, pharetra pharetra enim malesuada. Aenean fermentum vulputate tempus."
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel nunc justo. Vestibulum blandit et sem quis auctor. Sed elementum, sapien sit amet posuere cursus, orci tortor feugiat enim, et hendrerit lacus lectus nec purus. Nunc sed nunc eget velit interdum pellentesque id vel erat. Nunc dictum libero mollis semper eleifend. Praesent hendrerit metus id justo ultrices, pharetra pharetra enim malesuada. Aenean fermentum vulputate tempus.",
+    topic_id: Topic.last.id
   )
 end
 
@@ -16,14 +26,22 @@ end
 
 puts "5 skills created"
 
-for i in 0..8
+for i in 1..8
   Portfolio.create!(
     title: "Portfolio title #{i}",
-    subtitle: "My greate service",
+    subtitle: "Ruby on Rails",
     body: "Proin dapibus nec erat sit amet consectetur.",
     main_image: "https://www.placehold.it/600x400",
     thumb_image: "https://www.placehold.it/350x200" 
     )
 end
+
+Portfolio.create!(
+    title: "Portfolio title #{i}",
+    subtitle: "Angular",
+    body: "Proin dapibus nec erat sit amet consectetur.",
+    main_image: "https://www.placehold.it/600x400",
+    thumb_image: "https://www.placehold.it/350x200" 
+)
 
 puts "9 portfolios created"
